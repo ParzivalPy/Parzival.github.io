3c0ef2cee4ddd7cdeb02acc38ba3848b89640102
ParzivalPy <120360026+ParzivalPy@users.noreply.github.com>
2024-10-02 20:53:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,223 @@
+let dataTemp = [];
+let dataJours = [];
+
+async function fetchCityInfo(city) {
+  const apiUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const results = data.results;
+
+    const villeInfoList = results.map((result) => {
+      return {
+        name: result.name,
+        adminDivision:
+          result.admin1 ||
+          result.admin2 ||
+          result.admin3 ||
+          result.admin4 ||
+          result.country,
+      };
+    });
+
+    return villeInfoList;
+  } catch (error) {
+    console.error("Error fetching city information:", error);
+    return [];
+  }
+}
+
+async function displayCityInfo(city) {
+  const villeInfoList = await fetchCityInfo(city);
+
+  if (villeInfoList.length === 0) {
+    console.log("No city information available.");
+    return;
+  }
+
+  comboChange(villeInfoList);
+}
+
+function cityClick() {
+  let cityToSearch = document.getElementById("cityName").value;
+  displayCityInfo(cityToSearch);
+}
+
+function comboChange(cityList) {
+  const comboList = document.getElementById("comboList");
+
+  while (comboList.firstChild) {
+    comboList.removeChild(comboList.firstChild);
+  }
+
+  cityList.forEach((ville, index) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = index;
+    optionElement.textContent = ville.name + ", " + ville.adminDivision;
+    comboList.appendChild(optionElement);
+  });
+}
+
+const inputChange = document.getElementById("cityName");
+
+inputChange.addEventListener("change", cityClick);
+
+const searchButton = document.getElementById("cityButton");
+
+searchButton.addEventListener("click", rechercher);
+
+async function rechercher() {
+  let city = document.getElementById("cityName").value;
+  const apiUrl =
+    "https://geocoding-api.open-meteo.com/v1/search?name=" +
+    city +
+    "&count=10&language=en&format=json";
+  try {
+    const cityNumber = obtenirValeurOptionSelectionnee();
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const results = data.results[cityNumber];
+
+    const cityLatitude = results["latitude"];
+    const cityLongitude = results["longitude"];
+
+    meteoApi(cityLatitude, cityLongitude);
+  } catch (error) {
+    console.log("Une erreur s'est produite : " + error);
+  }
+}
+
+async function meteoApi(lat, lon) {
+  const meteoApi =
+    "https://api.open-meteo.com/v1/forecast?latitude=" +
+    lat +
+    "&longitude=" +
+    lon +
+    "&hourly=temperature_2m&timezone=Europe%2FBerlin";
+
+  try {
+    const response = await fetch(meteoApi);
+    const data = await response.json();
+    const hourlyTemperature = data.hourly;
+
+    dataJours = [];
+    dataTemp = [];
+
+    for (let i = 0; i < 7; i++) {
+      const debut = i * 24;
+      const fin = debut + 24;
+      const sousListe = hourlyTemperature["temperature_2m"].slice(debut, fin);
+      dataTemp.push(sousListe);
+    }
+
+    for (let i = 0; i < 7; i++) {
+      const debut = i * 24;
+      const fin = debut + 24;
+      const sousListe = hourlyTemperature["time"].slice(debut, fin);
+      dataJours.push(sousListe);
+    }
+
+    console.log(dataJours);
+    console.log(dataTemp);
+
+    afficherGraph();
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+
+function obtenirValeurOptionSelectionnee() {
+  selectElement = document.getElementById("comboList");
+  var selectedIndex = selectElement.selectedIndex;
+  var selectedValue = selectElement.options[selectedIndex].value;
+  return selectedValue;
+}
+
+function afficherGraph() {
+  console.log(dataJours);
+  parseDate();
+  addData(graph, dataJours[0], dataTemp[0]);
+}
+
+function parseDate() {
+  for (i = 0; i < 7; i++) {
+    tempDate = new Date(dataJours[i][0]);
+    tempDay = tempDate.getDate();
+    tempMonth = tempDate.getMonth();
+    document.getElementById("daysButton" + (i + 1)).textContent =
+      tempDay + "/" + (tempMonth + 1);
+  }
+}
+
+const jour1 = document.getElementById("daysButton1");
+
+jour1.addEventListener("click", function () {
+  addData(graph, dataJours[0], dataTemp[0]);
+});
+
+const jour2 = document.getElementById("daysButton2");
+
+jour2.addEventListener("click", function () {
+  addData(graph, dataJours[1], dataTemp[1]);
+});
+
+const jour3 = document.getElementById("daysButton3");
+
+jour3.addEventListener("click", function () {
+  addData(graph, dataJours[2], dataTemp[2]);
+});
+
+const jour4 = document.getElementById("daysButton4");
+
+jour4.addEventListener("click", function () {
+  addData(graph, dataJours[3], dataTemp[3]);
+});
+
+const jour5 = document.getElementById("daysButton5");
+
+jour5.addEventListener("click", function () {
+  addData(graph, dataJours[4], dataTemp[4]);
+});
+
+const jour6 = document.getElementById("daysButton6");
+
+jour6.addEventListener("click", function () {
+  addData(graph, dataJours[5], dataTemp[5]);
+});
+
+const jour7 = document.getElementById("daysButton7");
+
+jour7.addEventListener("click", function () {
+  addData(graph, dataJours[6], dataTemp[6]);
+});
+const ctx = document.getElementById("myChart");
+
+const graph = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Temperatures",
+        data: [],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    layout: {
+      padding: {
+        down: 20,
+      },
+    },
+  },
+});
+
+function addData(chart, label, newData) {
+  chart.data.labels = label;
+  chart.data.datasets[0].data = newData;
+  console.log(newData);
+  console.log(label);
+  chart.update();
+}
